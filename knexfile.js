102df9ac0a80be6a63b661b0becdedc0e60beb8d
Nathan Venn <natevenn@gmail.com>
2016-11-11 09:34:30 -0700

    add knex, pg, and config knexfile.js

---
+++ knexfile.js
@@ -0,0 +1,23 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: 'postgres://localhost/photography
+    pool: {
+      min: 2,
+      max: 10
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL
+    pool: {
+      min: 2,
+      max: 10
+    }
+  }
+
+};
