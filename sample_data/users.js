50cde551e0cc2605d03cf106c5a795ce4988d6dd
Nathan Venn <natevenn@gmail.com>
2016-11-11 17:08:01 -0700

    seed sample_data

---
+++ sample_data/users.js
@@ -0,0 +1,7 @@
+module.exports = [
+  {
+    id: 1,
+    username: 'john doe',
+    password: '$2a$10$bP2LsoRB4r8kSaczW75jjunetKSZNHUXsHCD6qA0/FeKMYztTnVfe'
+  }
+]
--- seeds/seed_data.js
+++ seeds/seed_data.js
@@ -1,40 +1,31 @@
+var users = require('../sample_data/users');
+var images = require('../sample_data/images');
+var collections = require('../sample_data/collections');
 exports.seed = (knex, Promise) => {
-  return Promise.join(
-    knex('users').del(),
-    knex('collections').del(),
-    knex('images').del(),
-
-    knex('users').insert(
-      {
-        id: 1,
-        username: 'Tereza Venn',
-        password: '$2a$10$bP2LsoRB4r8kSaczW75jjunetKSZNHUXsHCD6qA0/FeKMYztTnVfe'
-      }
-    ),
-
-    knex('collections').insert(
-      [
-        {
-          id: 1,
-          name: 'people'
-        },
-        {
-          id: 2,
-          name: 'places'
-        },
-        {
-          id: 3,
-          name: 'life'
-        },
-        {
-          id: 4,
-          name: 'art'
-        },
-        {
-          id: 5,
-          name: 'crested butte'
-        }
-      ]
-    )
-  )
+  return knex.transaction(function (t) {
+    return knex('images')
+    .del()
+    .then(function() {
+      return knex('collections').
+        del()
+    }).then(function() {
+      return knex('users').
+        del()
+    }).then(function() {
+      return knex('users')
+      .transacting(t)
+      .insert(users)
+    }).then(function (response) {
+      return knex('collections')
+      .transacting(t)
+      .insert(collections)
+    }).then(function (response) {
+      return knex('images')
+      .transacting(t)
+      .insert(images)
+    })
+    .then(t.commit)
+    .catch(t.rollback)
+  })
 };
+
