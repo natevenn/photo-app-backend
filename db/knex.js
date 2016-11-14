14dd20aa096aad8e87ba9ffcbecff927736b14b8
Nathan Venn <natevenn@gmail.com>
2016-11-14 16:33:06 -0700

    debug config in db/knex.js

--- db/knex.js
+++ db/knex.js
@@ -1,4 +1,7 @@
+var debug = require('debug')('app:config')
+
 var env = process.env.NODE_ENV || 'development';
 var config = require('../knexfile.js')[env];
+debug(config);
 module.exports = require('knex')(config);
 
