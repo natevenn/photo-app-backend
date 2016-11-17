182ac7087adc9c62b6c3445ede2f7597cf6ead31
Nathan Venn <natevenn@gmail.com>
2016-11-17 10:44:03 -0700

    refactor signup post

--- routes/api/v1/signup.js
+++ routes/api/v1/signup.js
@@ -5,25 +5,45 @@ router.post('/', (req, res, next) => {
   var db = req.app.get('db')
   var user = req.body
 
-  db('users').where({username: user.username})
-  .then( (existingUser) => {
-    if(existingUser[0] !== undefined) {
-      return res.json({errorMessage: 'Username already exists'})
-    } else {
-      if(user.username !== undefined && user.password !== undefined) {
+  if(user.username && user.password) {
+    db('users').where({username: user.username})
+    .then( (existingUser) => {
+      if(existingUser[0]) {
+        return res.json({errorMessage: 'Username already exists'})
+      } else {
         //hash password with bcrypt before saving
         db('users').insert(user)
         .then( () => {
+          //generate JWT
           //return JWT
           return res.json()
         });
-      } else {
-        var err = new Error('Not found');
-        err.status = 400;
-        next(err);
       }
+    });
+    }else{
+      var err = new Error('Not found');
+      err.status = 400;
+      next(err);
     }
+    //db('users').where({username: user.username})
+    //.then( (existingUser) => {
+    //if(existingUser[0] !== undefined) {
+    //return res.json({errorMessage: 'Username already exists'})
+    //} else {
+    //if(user.username !== undefined && user.password !== undefined) {
+    ////hash password with bcrypt before saving
+    //db('users').insert(user)
+    //.then( () => {
+    ////return JWT
+    //return res.json()
+    //});
+    //} else {
+    //var err = new Error('Not found');
+    //err.status = 400;
+    //next(err);
+    //}
+    //}
+    //});
   });
-});
 
-module.exports = router;
+  module.exports = router;
