152a3d25a0394d4ea888b0181b35df52f3e9c8c2
Nathan Venn <natevenn@gmail.com>
2016-11-17 10:57:35 -0700

    delete commented out code

--- routes/api/v1/signup.js
+++ routes/api/v1/signup.js
@@ -25,25 +25,6 @@ router.post('/', (req, res, next) => {
       err.status = 400;
       next(err);
     }
-    //db('users').where({username: user.username})
-    //.then( (existingUser) => {
-    //if(existingUser[0] !== undefined) {
-    //return res.json({errorMessage: 'Username already exists'})
-    //} else {
-    //if(user.username !== undefined && user.password !== undefined) {
-    ////hash password with bcrypt before saving
-    //db('users').insert(user)
-    //.then( () => {
-    ////return JWT
-    //return res.json()
-    //});
-    //} else {
-    //var err = new Error('Not found');
-    //err.status = 400;
-    //next(err);
-    //}
-    //}
-    //});
   });
 
   module.exports = router;
